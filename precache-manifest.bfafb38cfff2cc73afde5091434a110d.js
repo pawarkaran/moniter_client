4997adb6de14606edf4aedaceecee2cbe4f0d825
pawarkaran <pawarkaran2908@gmail.com>
2020-10-15 20:43:18 +0530

    Updates

---
+++ precache-manifest.bfafb38cfff2cc73afde5091434a110d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0042d02df5b2a202a67b7b2b327ed6",
+    "url": "/moniter_client/index.html"
+  },
+  {
+    "revision": "65367d41b5b9180e3641",
+    "url": "/moniter_client/static/css/2.2d5b2920.chunk.css"
+  },
+  {
+    "revision": "8de98d8779a7ffbf82dc",
+    "url": "/moniter_client/static/css/3.1c082f95.chunk.css"
+  },
+  {
+    "revision": "13d50baa23d39226cf45",
+    "url": "/moniter_client/static/css/main.bc1e5782.chunk.css"
+  },
+  {
+    "revision": "917368bb3596804f7877",
+    "url": "/moniter_client/static/js/10.c4ad4576.chunk.js"
+  },
+  {
+    "revision": "e719ced39aeb1b38255e",
+    "url": "/moniter_client/static/js/11.3848df0b.chunk.js"
+  },
+  {
+    "revision": "842bd08a1ef642031723",
+    "url": "/moniter_client/static/js/12.b6a15f83.chunk.js"
+  },
+  {
+    "revision": "955b28c808b5a7e98a97",
+    "url": "/moniter_client/static/js/13.6bbdacd4.chunk.js"
+  },
+  {
+    "revision": "b48aac5521a63613cea0",
+    "url": "/moniter_client/static/js/14.74733d96.chunk.js"
+  },
+  {
+    "revision": "9e0a0186531e0256dd82",
+    "url": "/moniter_client/static/js/15.849e4044.chunk.js"
+  },
+  {
+    "revision": "65367d41b5b9180e3641",
+    "url": "/moniter_client/static/js/2.88fbec9c.chunk.js"
+  },
+  {
+    "revision": "5db0532d8a40361a4583c5b38afbb736",
+    "url": "/moniter_client/static/js/2.88fbec9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de98d8779a7ffbf82dc",
+    "url": "/moniter_client/static/js/3.6c603086.chunk.js"
+  },
+  {
+    "revision": "06e36aa55294a6ca85c1",
+    "url": "/moniter_client/static/js/4.abf066b4.chunk.js"
+  },
+  {
+    "revision": "12ed51f1467f46142fd5",
+    "url": "/moniter_client/static/js/5.2dda87a9.chunk.js"
+  },
+  {
+    "revision": "b92c0f7a2f89fb63bcfd",
+    "url": "/moniter_client/static/js/6.6c59211c.chunk.js"
+  },
+  {
+    "revision": "546c6d9c56d7a3dc61b1",
+    "url": "/moniter_client/static/js/7.9bbc77ed.chunk.js"
+  },
+  {
+    "revision": "733058fd1fb7881e15fa",
+    "url": "/moniter_client/static/js/8.6fa22755.chunk.js"
+  },
+  {
+    "revision": "401ee27050c1adaf1481",
+    "url": "/moniter_client/static/js/9.87dccb02.chunk.js"
+  },
+  {
+    "revision": "13d50baa23d39226cf45",
+    "url": "/moniter_client/static/js/main.ff4960b1.chunk.js"
+  },
+  {
+    "revision": "ad552f362418f084d884",
+    "url": "/moniter_client/static/js/runtime-main.f453972d.js"
+  }
+]);
